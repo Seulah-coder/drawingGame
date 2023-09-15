89689aeb909f0cbd7d3e05acb47b8062281e4536
Seulah Kim <seulahkim@chunjae.co.kr>
2023-09-15 17:33:48 +0900

    Initial commit

---
+++ src/test/java/com/personal/drawingGame/DrawingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.drawingGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawingGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
